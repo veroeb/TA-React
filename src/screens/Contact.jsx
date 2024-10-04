429c7ad9c8c99e0be7f1283f9746fefa05a3f755
veroeb <vecheza@gmail.com>
2024-10-04 19:57:27 -0300

    ta16

---
+++ src/screens/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = ()  =>{
+  return (
+    <div>
+      <h1>Contactanos</h1>
+      <p>Podes ponerte en contacto con nosotros a través de esta página</p>
+    </div>
+  );
+}
+
+export default Contact;
